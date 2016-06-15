13a80d52e226d8a42473293dd6baf57a7d4f870c
g-sam <samgalson@gmail.com>
2016-06-15 18:13:00 +0100

    added svg data

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,21 +1,17 @@
 (function () {
 	'use strict';
-	setTimeout(function(){
-		location.reload()
-	}, 5000)
+	describe('stopwatch', function () {
 
-	describe('stopwatch test set', function () {
+		describe('should be able to', function () {
 
-		describe('maybe a bit more context here', function () {
-
-			it('should start and stop', function(){
+			it('start and stop', function(){
 				Watch.start();
 				expect(Watch.isRunning).toBe(1);
 				Watch.stop();
 				expect(Watch.isRunning).toBe(0);
 			})
 
-			it('should measure the time elapsed', function(done){
+			it('measure the time elapsed', function(done){
 				Watch.start();
 				setTimeout(function(){
 					Watch.stop(); 
@@ -23,7 +19,7 @@
 					done();
 				},50)
 			})
-			it('should have the option to either be restarted from 0 (reset) or restarted at the last stopped time', function(done){
+			it('either be restarted from 0 (reset) or restarted at the last stopped time', function(done){
 				Watch.start();
 				setTimeout(function(){
 					Watch.start()
@@ -36,6 +32,29 @@
 				console.log('here 2:',Watch.timeElapsed)
 
 			})
+			it('return the time elapsed when partial time button is clicked it', function(done){
+				Watch.start();
+				setTimeout(function(){
+					expect(Watch.lap()>=Watch.timeElapsed).toBe(true);
+					done()
+					Watch.stop()
+				},50)
+			})
+			it('store successive laps and return them in an array', function(done){
+				Watch.start();
+				var id = setInterval(function(){
+					Watch.lap()
+				},50);
+				setTimeout(function(){
+					clearInterval(id);
+					for (var i = 0; i < 5; i++){
+						expect(Watch.lapArray[i]>=50*i).toBe(true);
+					}
+					done();
+					Watch.stop();
+				}, 500);
+			})
+
 		});
 	});
 })();
